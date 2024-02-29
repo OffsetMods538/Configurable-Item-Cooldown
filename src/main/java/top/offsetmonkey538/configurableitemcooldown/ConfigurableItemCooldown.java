3b4c459ec07efbb888619b6770330fefc2da8000
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-02-29 18:54:27 +0200

    Add config

--- src/main/java/top/offsetmonkey538/configurableitemcooldown/ConfigurableItemCooldown.java
+++ src/main/java/top/offsetmonkey538/configurableitemcooldown/ConfigurableItemCooldown.java
@@ -4,14 +4,18 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import top.offsetmonkey538.configurableitemcooldown.config.ModConfig;
+import top.offsetmonkey538.monkeylib538.config.ConfigManager;
 
 public class ConfigurableItemCooldown implements ModInitializer {
 	public static final String MOD_ID = "configurable-item-cooldown";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static ModConfig config;
 
 	@Override
 	public void onInitialize() {
-		// Do stuff
+		config = ConfigManager.init(new ModConfig(), LOGGER::error);
+		LOGGER.info(config.test);
 	}
 
 	public static Identifier id(String path) {
---
+++ src/main/java/top/offsetmonkey538/configurableitemcooldown/config/ModConfig.java
@@ -0,0 +1,15 @@
+package top.offsetmonkey538.configurableitemcooldown.config;
+
+import top.offsetmonkey538.monkeylib538.config.Config;
+
+import static top.offsetmonkey538.configurableitemcooldown.ConfigurableItemCooldown.MOD_ID;
+
+public class ModConfig extends Config {
+
+    public String test = "e";
+
+    @Override
+    protected String getName() {
+        return MOD_ID;
+    }
+}
